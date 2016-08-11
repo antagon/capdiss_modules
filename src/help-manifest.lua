49edf76ff6fc6b4ca737acf7a03e34010688492e
antagon <antagon@codeward.org>
2016-08-11 15:39:50 +0200

    help: update description.

--- src/help-manifest.lua
+++ src/help-manifest.lua
@@ -3,6 +3,6 @@ return {
 	version = "1.0",
 	license = "GPLv2",
 	author = "antagon <antagon@codeward.org>",
-	description = "Show available options for a module.",
+	description = "Show options for a module.",
 	usage = "help <name>"
 }
