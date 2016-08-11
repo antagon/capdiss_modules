b6d2e94d1b5e5ba6f4b67efeff647ff315324c27
antagon <antagon@codeward.org>
2016-08-11 02:31:13 +0200

    modinfo: look inside a manifest file.
    Do not do file IO to find out if a module exists, instead, load a
    manifest file and see if there's a key matching the module name.

--- src/modinfo.lua
+++ src/modinfo.lua
@@ -4,17 +4,13 @@ if #arg < 1 then
 	error ("Missing a module name.")
 end
 
-local ifile = arg[1] .. "-manifest"
+local manifest = require ("manifest")
 
-local fd = io.open (ifile .. ".lua")
-
-if not fd then
+if not manifest[arg[1]] then
 	error (("Module '%s' not found."):format (arg[1]))
 end
 
-fd:close ()
-
-local manifest = require (ifile)
+local mod_manifest = dofile (manifest[arg[1]])
 
 io.write (([[
 Name: %s
@@ -23,11 +19,11 @@ Author: %s
 License: %s
 Description: %s
 ]]):format (
-	manifest.name or "not-specified",
-	manifest.license or "not-specified",
-	manifest.version or "not-specified",
-	manifest.author or "not-specified",
-	manifest.description or "not-specified"
+	mod_manifest.name or "not-specified",
+	mod_manifest.license or "not-specified",
+	mod_manifest.version or "not-specified",
+	mod_manifest.author or "not-specified",
+	mod_manifest.description or "not-specified"
 ))
 
 return modinfo
