3c5ee627bfd6e1a9c7077c0b359844a9bc16e1f1
antagon <antagon@codeward.org>
2016-08-11 15:40:09 +0200

    modlist: change delimiter between name and desc.

--- src/modlist.lua
+++ src/modlist.lua
@@ -5,7 +5,7 @@ local manifest = require ("manifest")
 for _, mod_manifest in pairs (manifest) do
 	local mod = dofile (mod_manifest)
 
-	io.write (("%s :: %s\n"):format (mod.name, mod.description or "no description available"))
+	io.write (("%s - %s\n"):format (mod.name, mod.description or "no description available"))
 end
 
 return modlist
