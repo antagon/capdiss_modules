6936e3aaed5626ace09ff0e1428216900cbb0417
antagon <antagon@codeward.org>
2016-08-11 15:40:30 +0200

    search: a new module for searching a module.

---
+++ src/search-manifest.lua
@@ -0,0 +1,14 @@
+return {
+	name = "search",
+	version = "1.0",
+	license = "GPLv2",
+	author = "antagon <antagon@codeward.org>",
+	description = "Search for a module by string.",
+	usage = "search [options] <string>",
+	options = {
+		"-n match only the module names",
+		"-d match only text in module descriptions",
+		"-a match only author names",
+		"-l match only the license names"
+	}
+}
---
+++ src/search.lua
@@ -0,0 +1,53 @@
+require ("getopt")
+require ("coroner")
+local search = {}
+
+local search_opt = ""
+local match_string = nil
+
+for opt, arg in getopt ("ndal", arg) do
+	if not opt then
+		match_string = arg
+	elseif opt == "?" then
+		error ("Undefined option '-" .. arg .. "'.")
+	else
+		search_opt = opt
+	end
+end
+
+if not match_string then
+	error ("Missing a string to match.")
+end
+
+if #search_opt == 0 then
+	search_opt = "nd"
+end
+
+local manifest = require ("manifest")
+
+for _, mod_manifest in pairs (manifest) do
+	local mod = dofile (mod_manifest)
+	local field = ""
+
+	for i = 1, #search_opt do
+		if search_opt:sub (i, i) == "n" then
+			field = mod.name
+		elseif search_opt:sub (i, i) == "d" then
+			field = mod.description
+		elseif search_opt:sub (i, i) == "a" then
+			field = mod.author
+		elseif search_opt:sub (i, i) == "l" then
+			field = mod.license
+		end
+
+		local match_beg, match_end = string.find (field:lower (), match_string:lower ())
+
+		if match_beg then
+			io.write (("%s - %s\n"):format (mod.name, mod.description))
+			break
+		end
+	end
+end
+
+return search
+
