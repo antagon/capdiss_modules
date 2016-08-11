ee4409636d4bfdc6922c062c9a94f323b97516df
antagon <antagon@codeward.org>
2016-08-11 02:24:42 +0200

    modinfo & modlist: license info.

--- src/modinfo-manifest.lua
+++ src/modinfo-manifest.lua
@@ -1,6 +1,7 @@
 return {
 	name = "modinfo",
 	version = "1.0",
+	license = "GPLv2",
 	author = "antagon <antagon@codeward.org>",
 	description = "Show information about a module.",
 	usage = "modinfo <name>"
--- src/modlist-manifest.lua
+++ src/modlist-manifest.lua
@@ -1,6 +1,7 @@
 return {
 	name = "modlist",
 	version = "1.0",
+	license = "GPLv2",
 	author = "antagon <antagon@codeward.org>",
 	description = "List installed modules."
 }
