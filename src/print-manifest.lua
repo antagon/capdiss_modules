6c0ee0561d7e5751597e50bc6c51f1a4c373b136
antagon <antagon@codeward.org>
2016-08-11 02:23:55 +0200

    print: license info + usage + options.

--- src/print-manifest.lua
+++ src/print-manifest.lua
@@ -1,7 +1,11 @@
 return {
 	name = "print",
 	version = "1.0",
+	license = "GPLv2",
 	author = "antagon <antagon@codeward.org>",
 	description = "Pretty-print packets.",
-	usage = "print"
+	usage = "print [options]",
+	options = {
+		"-e show Ethernet header"
+	}
 }
