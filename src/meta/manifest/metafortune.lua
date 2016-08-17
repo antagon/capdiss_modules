8ed0045686389271098b0b113f26e49e9f75941e
antagon <antagon@codeward.org>
2016-08-18 01:42:19 +0200

    meta: meta module to download all capdiss modules.

---
+++ src/meta/manifest/metafortune.lua
@@ -0,0 +1,6 @@
+return {
+	name = "metafortune",
+	version = "1.0",
+	license = "GPLv2",
+	author = "antagon <antagon@codeward.org>"
+}
---
+++ src/meta/src/metafortune.lua
@@ -0,0 +1,14 @@
+print ([[
+So I tell you the truth, really
+Me nah gon' need nuttin else
+But health, wealth, and knowledge of myself
+Me nah gon' need nuttin else
+But health, wealth, and knowledge of myself
+
+In the beginning was the word, the word was made flesh
+Knowledge K. Reigns R. Supreme S
+Some of us guess while others of us are blessed
+Take heed to the word, that I manifest
+I manifest the future, the present, followed by the past
+Everything in nature, rules by kickin ass
+]])
