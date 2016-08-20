b1dfb0f1666887b4a4d75a8e628c5ba7b0b2aa53
antagon <antagon@codeward.org>
2016-08-20 19:24:32 +0200

    print: convert floats to int.

--- src/print/src/print/format.lua
+++ src/print/src/print/format.lua
@@ -1,8 +1,11 @@
+local math = require ("math")
+local os = require ("os")
+
 function format_pktnum (num)
 	return ("%06d"):format (num)
 end
 
 function format_timestamp (ts)
-	return ("%s.%06d"):format (os.date ("%H:%M:%S", ts), (ts % 1) * 1000000)
+	return ("%s.%06d"):format (os.date ("%H:%M:%S", math.floor (ts)), math.floor ((ts % 1) * 1000000))
 end
 
