346d2d5270acf4025c453af12d1aaa994d449723
antagon <antagon@codeward.org>
2016-08-24 17:26:46 +0200

    modlist: support submodule manifests.

--- src/core/src/modlist.lua
+++ src/core/src/modlist.lua
@@ -1,20 +1,47 @@
 local lfs = require ("lfs")
 
-local modlist = {}
+local function merge_tables (t1, t2)
+	for _, v in pairs (t2) do
+		table.insert (t1, v)
+	end
+
+	return t1
+end
+
+local function find_manifests (dirname, prefix)
+	local files = {}
+
+	if prefix then
+		prefix = prefix .. "/"
+	else
+		prefix = ""
+	end
+
+	for file in lfs.dir (dirname) do
+		if file ~= "." and file ~= ".." then
+			local path = dirname .. "/" .. file
+			local ftype = lfs.attributes (path, "mode")
+
+			if ftype == "file" then
+				table.insert (files, prefix .. file)
+			elseif ftype == "directory" then
+				merge_tables (files, find_manifests (path, prefix .. file))
+			end
+		end
+	end
+
+	return files
+end
 
 local manifest = require ("manifest")
 
-for file in lfs.dir (manifest:dir_path ()) do
-	file = manifest:dir_path () .. "/" .. file
+for _, file in ipairs (find_manifests (manifest:dir_path ())) do
+	file = file:gsub (".lua$", "")
 
-	if lfs.attributes (file, "mode") == "file" then
-		if not manifest:loadfile (file) then
-			error (("Cannot load manifest file '%s'"):format (file))
-		end
-
-		io.write (("%s - %s\n"):format (manifest:get_name (), manifest:get_description () or "no description available"))
+	if not manifest:load (file) then
+		error (("Cannot load manifest file for module '%s': %s"):format (file, manifest:get_error ()))
 	end
-end
 
-return modlist
+	io.write (("%s - %s\n"):format (manifest:get_name (), manifest:get_description () or "no description available"))
+end
 
